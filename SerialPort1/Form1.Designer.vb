b80f1fdbde0c0b7b7eb51d6a3d3d22a91a3c6118
nasalapao <nasalapao@gmail.com>
2024-03-12 09:44:38 +0700

    Add project files.

---
+++ SerialPort1/Form1.Designer.vb
@@ -0,0 +1,59 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Label1 = New Label()
+        Timer1 = New Timer(components)
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(23, 93)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(329, 128)
+        Label1.TabIndex = 0
+        Label1.Text = "Label1"
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Timer1 As Timer
+
+End Class
---
+++ SerialPort1/Form1.vb
@@ -0,0 +1,40 @@
+﻿Imports System.IO.Ports
+
+Public Class Form1
+    Dim SerialPort1 = New SerialPort()
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Connetc_Serial_Port()
+
+    End Sub
+
+    Private Sub Connetc_Serial_Port()
+        Try
+            ' Create a new SerialPort object with default settings.
+
+            With SerialPort1
+                .PortName = "COM2" 'COMPORT
+                .BaudRate = 9600
+                .Parity = Parity.None
+                .DataBits = 8
+                .StopBits = 1 'IO.Ports.StopBits.One
+
+                If .IsOpen = True Then .Close()
+            End With
+            SerialPort1.Open()
+
+        Catch ex As Exception
+            MsgBox(" <<<PORT >>>  ไม่พบหรือกำลังเปิดอยู่ กรุณาปิดโปรแกรมอื่นๆก่อนแล้วเปิดโปรแกรมนี้ใหม่อีกครั้ง", MsgBoxStyle.Information)
+            Application.Exit()
+        End Try
+
+    End Sub
+
+
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim message As String = SerialPort1.ReadLine()
+        Label1.Text = message
+        Console.WriteLine(message)
+    End Sub
+End Class
